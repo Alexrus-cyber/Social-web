28488ff4d9c2e55e2c0c9b1d4e0cc29e731e084d
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-11-07 21:19:07 +0300

    Add news

---
+++ src/components/News/News.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './News.module.css';
+
+
+const News = (props) => {
+    debugger
+    return (
+        <div className={styles.content}>
+
+        </div>
+    );
+}
+
+export default  News;
